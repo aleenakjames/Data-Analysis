d25185d3caa70f5a0e593c163f51a7cb0f1ca3a7
aleenakjames <crizjames1096@gmail.com>
2024-12-21 22:27:15 +0530

    Analysis modified

--- packages.R
+++ packages.R
@@ -1,3 +1,3 @@
 options(repos = c(CRAN = "https://cloud.r-project.org/"))
-install.packages(c("tidyverse", "caret", "class", "nnet", "rmarkdown", "tinytex", "Metrics", "C50"))
+install.packages(c("tidyverse", "caret", "class", "nnet", "rmarkdown", "tinytex", "Metrics", "C50", "ggplot2")) # nolint
 tinytex::install_tinytex()
