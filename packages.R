4b93602bdd25fffd2f1df290f1be0d4ff8a102d1
aleenakjames <crizjames1096@gmail.com>
2024-12-21 21:21:48 +0530

    Initial commit

---
+++ packages.R
@@ -0,0 +1,3 @@
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
+install.packages(c("tidyverse", "caret", "class", "nnet", "rmarkdown", "tinytex", "Metrics", "C50"))
+tinytex::install_tinytex()
